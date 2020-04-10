0a6d583449cc745515c4fc1b3a16d90316232c4b
kboroszko <kajetan.boroszko@gmail.com>
2020-04-10 17:10:22 +0200

    payload fix4

--- stats.c
+++ stats.c
@@ -32,7 +32,7 @@ int main(int argc, char * argv[])
 
     MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
     MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
-    int node = myRank/numProcesses;
+    int node = myRank/(numProcesses/2);
     int partnerNode, partnerRank;
     if(node == 0){
         partnerNode = 1;
