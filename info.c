eec76994fc9ad576b61bacb6236e9bc7963e14d1
kboroszko <kajetan.boroszko@gmail.com>
2020-04-10 15:35:11 +0200

    changed info.c back to printing whole name

--- info.c
+++ info.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     MPI_Get_processor_name(name, &len);
-    printf("Hello, world.  I am %d of %d on %c\n", rank, nprocs, name[len-1]);fflush(stdout);
+    printf("Hello, world.  I am %d of %d on %s\n", rank, nprocs, name);fflush(stdout);
     MPI_Finalize();
     return 0;
 }
